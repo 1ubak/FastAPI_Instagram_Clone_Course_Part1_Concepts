7fb78b6c4f02b6f7ef27275d10fdf64a6568d68b
1ubak <lubak@wearehackerone.com>
2022-08-23 17:37:47 +0300

    CRUD complete

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+
+import router.user
+from db import models
 from router import blog_get
 from router import blog_post
+from db.database import engine
+
 app = FastAPI()
+app.include_router(router.user.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
+
 
 @app.get('/')
 def index():
     return 'Hello World'
+
+
+models.Base.metadata.create_all(engine)
